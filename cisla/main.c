38d8a67055040aea5d0ad2511c60a1b2b3d55e26
pavelbarto <pavelbarto@gmail.com>
2016-10-05 15:51:29 +0200

    kuska ci sa to zapise

---
+++ cisla/main.c
@@ -0,0 +1,12 @@
+/*------------------------------------------------*/
+/* c05preklad/main.c                              */
+ 
+#include "number1.h"
+#include "number2.h"
+ 
+int main(void) {
+        number1();
+        number2();
+        return 0;
+}
+/*------------------------------------------------*/
---
+++ volby.c
@@ -0,0 +1,15 @@
+/*------------------------------------------------*/
+/* 03volby/volby.c                                */
+
+void main(void)
+{
+    int x, y, z;
+    char slovo[] = "Ahoj ";
+    // x nebylo inicializovano 
+    
+    y = x;
+    for (; y > x + 1; y++)
+	printf("%s %i\n", slovo, "svete!", x);
+    return;
+}
+/*------------------------------------------------*/
